c2451830a50944d1ac04824bcf8851eb016f3c59
Adedapo Adeyemi <adedapo025@gmail.com>
2025-09-20 21:18:16 +0100

    resolved cors for backend

--- backend/index.cjs
+++ backend/index.cjs
@@ -10,7 +10,7 @@ app.use(express.json())
 const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5500',
-  // Add your deployed frontend URL here
+  'https://time-consistent-event-scheduler.vercel.app/'
 ]
 
 app.use(cors({ origin: allowedOrigins }));
